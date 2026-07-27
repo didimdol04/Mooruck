6abe1de1ef9fddcaff00690f8e8d705e5094e28d
5ranke <wjdtnwls0106@gmail.com>
2026-07-28 00:07:04 +0900

    feat: AppDatabase 엔티티 추가

--- app/src/main/java/com/example/mooruckapp/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/mooruckapp/data/local/AppDatabase.kt
@@ -4,16 +4,22 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.mooruckapp.data.local.dao.UserPlantDao
+import com.example.mooruckapp.data.local.entity.UserPlant
 
 // Entity를 만든 뒤 아래 entities 배열에 추가
 @Database(
-    entities = [GrowthDiary::class], // TODO: 각 담당자가 자기 Entity 추가
+    entities = [
+        GrowthDiary::class,
+        UserPlant::class
+    ], // TODO: 각 담당자가 자기 Entity 추가
     version = 1,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun growthDiaryDao(): GrowthDiaryDao
+    abstract fun userPlantDao(): UserPlantDao
 
     companion object {
         @Volatile
