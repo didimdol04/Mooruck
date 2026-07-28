f7b2bb0cc1d14b764d23bde37440b1b5fbe5ea9c
5ranke <wjdtnwls0106@gmail.com>
2026-07-28 17:41:37 +0900

    feat: 응답 String형태로 받아오게

--- app/src/main/java/com/example/mooruckapp/network/api/PlantApiService.kt
+++ app/src/main/java/com/example/mooruckapp/network/api/PlantApiService.kt
@@ -1,7 +1,5 @@
 package com.example.mooruckapp.network.api
 
-import com.example.mooruckapp.network.dto.PlantDetailResponse
-import com.example.mooruckapp.network.dto.SearchPlantResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,15 +9,16 @@ interface PlantApiService {
     @GET("service/garden/gardenList")
     suspend fun searchPlants(
         @Query("apiKey") apiKey: String,
+        @Query("sType") searchType: String,
         @Query("sText") keyword: String,
         @Query("pageNo") pageNo: Int = 1,
         @Query("numOfRows") numOfRows: Int = 20
-    ): SearchPlantResponse
+    ): String
 
     // 식물 상세 조회
     @GET("service/garden/gardenDtl")
     suspend fun getPlantDetail(
         @Query("apiKey") apiKey: String,
         @Query("cntntsNo") contentNo: String
-    ): PlantDetailResponse
+    ): String
 }
