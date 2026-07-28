477c69f0057214bf8e8468e7320a101245032a0e
5ranke <wjdtnwls0106@gmail.com>
2026-07-28 17:38:57 +0900

    feat: 조회 응답 저장 dto

--- app/src/main/java/com/example/mooruckapp/network/dto/SearchPlantResponse.kt
+++ app/src/main/java/com/example/mooruckapp/network/dto/SearchPlantResponse.kt
@@ -1,41 +1,6 @@
 package com.example.mooruckapp.network.dto
 
-import com.google.gson.annotations.SerializedName
-
-data class SearchPlantResponse(
-
-    @SerializedName("response")
-    val response: Response
-
-)
-
-data class Response(
-
-    @SerializedName("body")
-    val body: Body
-
-)
-
-data class Body(
-
-    @SerializedName("items")
-    val items: Items
-
-)
-
-data class Items(
-
-    @SerializedName("item")
-    val item: List<PlantItem>
-
-)
-
 data class PlantItem(
-
-    @SerializedName("cntntsNo")
     val contentNo: String,
-
-    @SerializedName("cntntsSj")
-    val title: String
-
+    val title: String,
 )
